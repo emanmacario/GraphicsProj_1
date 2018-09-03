f79855a13324027e4ae24d7cdb540bc4929c8a62
rs-1 <rsam1@student.unimelb.edu.au>
2018-09-03 17:39:55 +1000

    Simplify camera code even further

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-    public float fwdSpeed=20.0f, yawSpeed=45.0f, pitchSpeed=45.0f;
-    private float fwdMag, yawMag, pitchMag;
+    public float translateSpeed=20, yawSpeed=45, pitchSpeed=45;
+    private float translateMag, yawMag, pitchMag;
     private Transform tf;
 
     public void Start() {
@@ -16,20 +16,15 @@ public class CameraControl : MonoBehaviour {
         /* Source: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html */
         yawMag = Input.GetAxis("Mouse X") * yawSpeed;
         pitchMag = Input.GetAxis("Mouse Y") * pitchSpeed;
-        tf.Rotate(0, yawMag, 0);
         tf.Rotate(-pitchMag, 0, 0);
+        tf.Rotate(0, yawMag, 0);
 
-        fwdMag = fwdSpeed * Time.deltaTime;
-        Vector3 normCamVec = tf.forward.normalized;
-
-        Vector3 sideVec = fwdMag * tf.TransformDirection(Vector3.right);
-        Vector3 fwdVec = fwdMag * normCamVec;
-
-        if (Input.GetKey("a")) { tf.localPosition -= sideVec; }
-        if (Input.GetKey("d")) { tf.localPosition += sideVec; }
-
-        if (Input.GetKey("s")) { tf.localPosition -= fwdVec; }
-        if (Input.GetKey("w")) { tf.localPosition += fwdVec; }
+        /* Source: https://docs.unity3d.com/ScriptReference/Transform.Translate.html */
+        translateMag = translateSpeed * Time.deltaTime;
+        if (Input.GetKey("a")) { tf.Translate(translateMag * Vector3.left); }
+        if (Input.GetKey("d")) { tf.Translate(translateMag * Vector3.right); }
+        if (Input.GetKey("s")) { tf.Translate(translateMag * Vector3.back); }
+        if (Input.GetKey("w")) { tf.Translate(translateMag * Vector3.forward); }
 
     }
 
