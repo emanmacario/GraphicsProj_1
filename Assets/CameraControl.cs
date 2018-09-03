c210ca9366ec0229485bd9ffdfc4e29eeee404ea
rs-1 <rsam1@student.unimelb.edu.au>
2018-09-03 17:27:29 +1000

    Cache local vars, shorten code

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,29 +3,33 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-    public float SPEED = 20.0f;
-    public float yawSpeed = 45.0f;
-    public float pitchSpeed = 45.0f;
+    public float fwdSpeed=20.0f, yawSpeed=45.0f, pitchSpeed=45.0f;
+    private float fwdMag, yawMag, pitchMag;
+    private Transform tf;
+
+    public void Start() {
+        tf = this.transform;
+    }
 
     public void Update() {
 
         /* Source: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html */
-        float yawMag = Input.GetAxis("Mouse X") * yawSpeed;
-        float pitchMag = Input.GetAxis("Mouse Y") * pitchSpeed;
-        this.transform.Rotate(0, yawMag, 0);
-        this.transform.Rotate(-pitchMag, 0, 0);
+        yawMag = Input.GetAxis("Mouse X") * yawSpeed;
+        pitchMag = Input.GetAxis("Mouse Y") * pitchSpeed;
+        tf.Rotate(0, yawMag, 0);
+        tf.Rotate(-pitchMag, 0, 0);
 
-        float dispMag = SPEED * Time.deltaTime;
-        Vector3 normCamVec = this.transform.forward.normalized;
+        fwdMag = fwdSpeed * Time.deltaTime;
+        Vector3 normCamVec = tf.forward.normalized;
 
-        Vector3 sideVec = dispMag * this.transform.TransformDirection(Vector3.right);
-        Vector3 fwdVec = dispMag * normCamVec;
+        Vector3 sideVec = fwdMag * tf.TransformDirection(Vector3.right);
+        Vector3 fwdVec = fwdMag * normCamVec;
 
-        if (Input.GetKey("a")) { this.transform.localPosition -= sideVec; }
-        if (Input.GetKey("d")) { this.transform.localPosition += sideVec; }
+        if (Input.GetKey("a")) { tf.localPosition -= sideVec; }
+        if (Input.GetKey("d")) { tf.localPosition += sideVec; }
 
-        if (Input.GetKey("s")) { this.transform.localPosition -= fwdVec; }
-        if (Input.GetKey("w")) { this.transform.localPosition += fwdVec; }
+        if (Input.GetKey("s")) { tf.localPosition -= fwdVec; }
+        if (Input.GetKey("w")) { tf.localPosition += fwdVec; }
 
     }
 
