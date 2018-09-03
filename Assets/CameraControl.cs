d5b7b3129929d341cb93aa5554caf7d12e087202
rs-1 <rsam1@student.unimelb.edu.au>
2018-09-03 16:48:58 +1000

    Add primitive flight sim control (alpha, see desc)
    Camera is only a navigation test. Do not base any work on this!
    - Mouse pitch + yaw
    - Forward/backward movement
    - No sideways movement
    - No terrain collision detection
    - No world bounding
    - No optimisations

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public float SPEED = 20.0f;
+    public float yawSpeed = 45.0f;
+    public float pitchSpeed = 45.0f;
+
+    public void Update() {
+
+        /* Source: https://docs.unity3d.com/ScriptReference/Input.GetAxis.html */
+        float yawMag = Input.GetAxis("Mouse X") * yawSpeed;
+        float pitchMag = Input.GetAxis("Mouse Y") * pitchSpeed;
+        this.transform.Rotate(0, yawMag, 0);
+        this.transform.Rotate(-pitchMag, 0, 0);
+
+        float dispMag = SPEED * Time.deltaTime;
+        Vector3 normCamVec = this.transform.forward.normalized;
+
+        /* Vector3 sideVec = dispMag * Vector3.Cross(Vector3.up, normCamVec); */
+        /* Vector3 sideVec = dispMag * (normCamVec.y > 0) ? Vector3.Cross(normCamVec, Vector3.up) : Vector3.Cross(Vector3.down, normCamVec); */
+        /* Vector3 sideVec = dispMag * Vector3.Cross(normCamVec, Vector3.forward); */
+
+        Vector3 fwdVec = dispMag * normCamVec;
+
+        /* if (Input.GetKey("a")) { this.transform.localPosition -= sideVec; } */
+        /* if (Input.GetKey("d")) { this.transform.localPosition += sideVec; } */
+
+        if (Input.GetKey("s")) { this.transform.localPosition -= fwdVec; }
+        if (Input.GetKey("w")) { this.transform.localPosition += fwdVec; }
+
+    }
+
+}
