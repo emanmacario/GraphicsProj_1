94e8e4b8fc3854a7f32573e7c1b223a188547d78
rs-1 <rsam1@student.unimelb.edu.au>
2018-09-04 19:52:49 +1000

    Bind camera within landscape

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-    public float translateSpeed=20, yawSpeed=45, pitchSpeed=45;
+    public float translateSpeed=5, yawSpeed=45, pitchSpeed=45;
     private float translateMag, yawMag, pitchMag;
     private Transform tf;
 
@@ -26,6 +26,18 @@ public class CameraControl : MonoBehaviour {
         if (Input.GetKey("o") || Input.GetKey("s")) { tf.Translate(translateMag * Vector3.back); }
         if (Input.GetKey(",") || Input.GetKey("w")) { tf.Translate(translateMag * Vector3.forward); }
 
+        boundInWorld();
+
+    }
+
+    private void boundInWorld() {
+        Vector3 pos = tf.localPosition;
+        float x=pos.x, y=pos.y, z=pos.z;
+        float sqRadius = 5;
+        x = (x < -sqRadius) ? -sqRadius : (sqRadius < x) ? sqRadius : x;
+        y = (y < 0) ? 0 : y;
+        z = (z < -sqRadius) ? -sqRadius : (sqRadius < z) ? sqRadius : z;
+        tf.localPosition = new Vector3(x, y, z);
     }
 
 }
