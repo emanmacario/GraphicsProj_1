cbe9ff333b205b713d84f79d3d2b4467b3866b37
rs-1 <rsam1@student.unimelb.edu.au>
2018-09-04 19:27:27 +1000

    Add orbiting cube

---
+++ Assets/Scripts/Sun.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Sun : MonoBehaviour {
+
+    public float period=8, orbitRadius=10;
+    private float time, timeToRads, timeToDegs, rads, degs;
+    private Transform tf;
+
+    public void Start() {
+        time = 0;
+        timeToRads = (float) (2 * Math.PI / period);
+        timeToDegs = (float) (360 / period);
+        tf = this.transform;
+        tf.localPosition = new Vector3(orbitRadius, 0, 0);
+    }
+
+    public void Update() {
+        time += Time.deltaTime;
+        while (period < time) { time -= period; }
+        rads = time * timeToRads;
+        degs = time * timeToDegs;
+        tf.localPosition = orbitRadius * new Vector3((float) Math.Cos(rads), (float) Math.Sin(rads), 0);
+        tf.rotation = Quaternion.AngleAxis(degs, Vector3.forward);
+    }
+
+}
